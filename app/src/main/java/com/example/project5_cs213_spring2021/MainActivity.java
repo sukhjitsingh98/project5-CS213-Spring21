a5c8e563181e2d7f3521e923de6b3c4da3a9e57e
Sukhjit Singh <ssukhjit1998@gmail.com>
2021-04-06 15:31:03 -0400

    Created new android project

---
+++ app/src/main/java/com/example/project5_cs213_spring2021/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.project5_cs213_spring2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
