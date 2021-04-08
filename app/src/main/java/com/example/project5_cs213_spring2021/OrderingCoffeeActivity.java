578d38e41835c250ac92b187791f77e6546d57cf
Sukhjit Singh <ssukhjit1998@gmail.com>
2021-04-08 15:21:12 -0400

    Added Dropdown menu and code for ordering coffee

--- app/src/main/java/com/example/project5_cs213_spring2021/OrderingCoffeeActivity.java
+++ app/src/main/java/com/example/project5_cs213_spring2021/OrderingCoffeeActivity.java
@@ -3,6 +3,8 @@ package com.example.project5_cs213_spring2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class OrderingCoffeeActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class OrderingCoffeeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ordering_coffee);
+
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.coffeeSize_spinner);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"Short", "Tall", "Grande", "Venti"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
     }
 }
