6a66e8aed1acc8611c59f45bdf6b631995ddbab9
GermanMunguia <munguiagerman48@gmail.com>
2021-04-08 12:47:19 -0400

    Created the homepage design with imageButtons

--- app/src/main/java/com/example/project5_cs213_spring2021/MainActivity.java
+++ app/src/main/java/com/example/project5_cs213_spring2021/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.project5_cs213_spring2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton orderCoffee;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
