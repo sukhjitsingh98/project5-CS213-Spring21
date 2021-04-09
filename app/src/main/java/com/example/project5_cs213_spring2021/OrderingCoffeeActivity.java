e9c961e761618542a04e61876e60635e64f8d7e5
Sukhjit Singh <ssukhjit1998@gmail.com>
2021-04-08 22:54:47 -0400

    Added code for count dropbox and revised code for size drop down in ordering coffee class

--- app/src/main/java/com/example/project5_cs213_spring2021/OrderingCoffeeActivity.java
+++ app/src/main/java/com/example/project5_cs213_spring2021/OrderingCoffeeActivity.java
@@ -2,24 +2,54 @@ package com.example.project5_cs213_spring2021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class OrderingCoffeeActivity extends AppCompatActivity {
+
+    String size = "Short";
+    int quantity = 0;
+    Coffee coffee = new Coffee(size, quantity);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ordering_coffee);
 
-        //get the spinner from the xml.
-        Spinner dropdown = findViewById(R.id.coffeeSize_spinner);
-        //create a list of items for the spinner.
-        String[] items = new String[]{"Short", "Tall", "Grande", "Venti"};
-        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        //set the spinners adapter to the previously created one.
-        dropdown.setAdapter(adapter);
+        //Dropdown Menu for Coffee Type (Size)
+        Spinner coffeeSizeDropdown = findViewById(R.id.coffeeSize_spinner);
+        ArrayAdapter<String> coffeeSizeAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                getResources().getStringArray(R.array.coffeeSizes));
+        coffeeSizeDropdown.setAdapter(coffeeSizeAdapter);
+
+        coffeeSizeDropdown.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                size = coffeeSizeDropdown.getItemAtPosition(position).toString();
+                coffee.setCoffeeType(size);
+            }
+        });
+
+        //Dropdown Menu for Coffee Quantity
+        Spinner coffeeQuantityDropdown = findViewById(R.id.coffeeQuantity_spinner);
+        ArrayAdapter<String> coffeeQuantityAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                getResources().getStringArray(R.array.coffeeCount));
+        coffeeQuantityDropdown.setAdapter(coffeeQuantityAdapter);
+
+        coffeeQuantityDropdown.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                quantity = Integer.parseInt(coffeeQuantityDropdown.getItemAtPosition(position).toString());
+                coffee.setCoffeeQuantity(quantity);
+            }
+        });
     }
 }
