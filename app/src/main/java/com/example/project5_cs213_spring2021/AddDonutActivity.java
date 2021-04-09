17e717ae99f208b7d7716c8131c3e457543d2b3b
GermanMunguia <munguiagerman48@gmail.com>
2021-04-09 14:46:57 -0400

    activity_add_donut also shows and updates price on selection.

--- app/src/main/java/com/example/project5_cs213_spring2021/AddDonutActivity.java
+++ app/src/main/java/com/example/project5_cs213_spring2021/AddDonutActivity.java
@@ -3,6 +3,7 @@ package com.example.project5_cs213_spring2021;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -25,9 +26,22 @@ public class AddDonutActivity extends AppCompatActivity {
         String flavor = getIntent().getStringExtra(String.valueOf(R.string.flavor));
         flavorName.setText(flavor);
 
+        //Set up an onclick listener which will change the price depending on the count selection.
+        countSpinner = (Spinner) findViewById(R.id.countSpinner);
+        countSpinner.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        int count = Integer.parseInt(countSpinner.getSelectedItem().toString());
+                        //get the textview to modify
+                        price = (TextView) findViewById(R.id.donutPrice);
+                        price.setText(getResources().getString(R.string.dollar_sign) + String.format("%.2f", count*Constants.YEAST_DONUT_PRICE));
+                    }
+                    public void onNothingSelected(AdapterView<?> parent) { //selected by default, this does nothing but is still needed.
+                    }
+                });
     }
 
-
+    //return the count to the previous activity.
     public void handleAddDonut(View view) {
         //get the number that was chosen.
         countSpinner = (Spinner) findViewById(R.id.countSpinner);
